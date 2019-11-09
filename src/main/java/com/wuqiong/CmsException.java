68d569fe1b8d1b0a34274853b5788886cf7a4852
wuqiongcs <1213983745@qq.com>
2019-11-09 11:16:57 +0800

    工具类

---
+++ src/main/java/com/wuqiong/CmsException.java
@@ -0,0 +1,14 @@
+package com.wuqiong;
+
+public class CmsException extends Exception{
+
+	public CmsException(String string) {
+		super(string);
+		
+	}
+	
+	private static final long serialVersionUID = -2388326871380439088L;
+	
+	
+	
+}
